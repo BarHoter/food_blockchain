cb865345797ad0a6270854c2d23c36811b7a0c00
gald33 <gald33@gmail.com>
2025-05-25 10:06:03 +0300

    basic test

---
+++ test/BatchToken.test.js
@@ -0,0 +1,22 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("BatchToken", function() {
+  let token, owner, addr1;
+
+  beforeEach(async () => {
+    [owner, addr1] = await ethers.getSigners();
+    const Factory = await ethers.getContractFactory("BatchToken");
+    token = await Factory.deploy();
+    await token.deployed();
+  });
+
+  it("emits TransferProposed correctly", async () => {
+    await expect(
+      token.proposeTransfer(42, addr1.address, 1_700_000_000)
+    )
+      .to.emit(token, "TransferProposed")
+      .withArgs(42, owner.address, addr1.address, 1_700_000_000);
+  });
+  // add more tests here…
+});
